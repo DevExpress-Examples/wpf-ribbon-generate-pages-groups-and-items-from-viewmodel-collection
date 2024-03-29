5f42c90e4c980ca97a1c983a854b213a2f026e6f
ivan <38530056+ivansupport@users.noreply.github.com>
2024-03-29 16:45:00 +0400

    c# refactoring

--- CS/Q521663/ViewModel.cs
+++ CS/Q521663/ViewModel.cs
@@ -1,67 +1,53 @@
-﻿using System;
+﻿using DevExpress.Mvvm;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Q521663
-{
-    public class ViewModel
-    {
-        public ViewModel()
-        {
-            ObservableCollection<ItemBase> items = new ObservableCollection<ItemBase>();
-            items.Add(new ItemBase() { Name = "Item1" });
-            items.Add(new ItemBase() { Name = "Item2" });
-
-            ObservableCollection<Group> groups = new ObservableCollection<Group>();
-            groups.Add(new Group() { Name = "Group1", Items = items });
-
-            ObservableCollection<Page> pages = new ObservableCollection<Page>();
-            pages.Add(new Page() { Name = "Page1", Groups = groups });
-            pages.Add(new Page() { Name = "Page2"});
-
-            Categories = new ObservableCollection<Category>();
-            Categories.Add(new Category() { Name = "Category1", Pages = pages });
+namespace Q521663 {
+    public class ViewModel {
+        public ViewModel() {
+            Categories = new ObservableCollection<Category> {
+                new Category("Category1",
+                    new Page("Page1",
+                        new Group("Group1",
+                            new Item("Item1"),
+                            new Item("Item2")
+                        )),
+                    new Page("Page2")
+                )
+            };
 
         }
-        public ObservableCollection<Category> Categories
-        {
-            get;
-            set;
+        public ObservableCollection<Category> Categories { get; }
+    }
+    public class Category : ItemBase {
+        public ObservableCollection<Page> Pages { get; }
+        public Category(string name = null, params Page[] pages) : base(name) {
+            Pages = new ObservableCollection<Page>(pages);
         }
     }
-    public class Category : ItemBase
-    {
-        public ObservableCollection<Page> Pages
-        {
-            get;
-            set;
+    public class Page : ItemBase {
+        public ObservableCollection<Group> Groups { get; }
+        public Page(string name = null, params Group[] groups) : base(name) {
+            Groups = new ObservableCollection<Group>(groups);
         }
     }
-    public class Page : ItemBase
-    {
-        public ObservableCollection<Group> Groups
-        {
-            get;
-            set;
+    public class Group : ItemBase {
+        public ObservableCollection<Item> Items { get; }
+        public Group(string name = null, params Item[] items) : base(name) {
+            Items = new ObservableCollection<Item>(items);
         }
     }
-    public class Group : ItemBase
-    {
-        public ObservableCollection<ItemBase> Items
-        {
-            get;
-            set;
-        }
+    public class Item : ItemBase {
+        public Item(string name = null) : base(name) { }
     }
-    public class ItemBase
-    {
-        public string Name
-        {
-            get;
-            set;
+    public abstract class ItemBase : ViewModelBase {
+        public string Name { get => GetValue<string>(); set => SetValue(value); }
+        public ItemBase(string name) {
+            Name = name;
         }
     }
 }
